adf8c534076dd08ba1bf1cd1ba64c5fe6d602f4d
Sheldon Trotman <Sheldon>
2015-07-06 13:57:10 -0400

    created and linked all XIBs

---
+++ Engo/Initial/Landing/LandingViewController.m
@@ -0,0 +1,37 @@
+//
+//  LandingViewController.m
+//  Engo
+//
+//  Created by Sheldon Trotman on 7/6/15.
+//  Copyright (c) 2015 Sheldon Productions. All rights reserved.
+//
+
+#import "LandingViewController.h"
+
+@interface LandingViewController ()
+
+@end
+
+@implementation LandingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Engo/Main/Events/EventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventsViewController.h
+//  Engo
+//
+//  Created by Sheldon Trotman on 7/6/15.
+//  Copyright (c) 2015 Sheldon Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventsViewController : UIViewController
+
+@end
